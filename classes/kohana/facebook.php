f534ddc1dd5fc82e7747bd31f765114b416eab67
Alex Tucker <tucker.al@gmail.com>
2012-04-11 18:45:15 -0400

    Making configurable through contructuor

--- classes/kohana/facebook.php
+++ classes/kohana/facebook.php
@@ -17,26 +17,38 @@ class Kohana_Facebook
 
 	protected $_me;
 
-	protected function __construct()
+	protected function __construct($config = null)
 	{
 		include Kohana::find_file('vendor', 'facebook/src/facebook');
-
-		// Do class setup
-		$this->_facebook = new Facebook(
-			array(
-				'appId'  => Kohana::config('facebook')->app_id,
-				'secret' => Kohana::config('facebook')->secret,
-				//'cookie' => true, // enable optional cookie support
-			)
-		);
+		
+		if($config == null) {
+			// Do class setup
+			$this->_facebook = new Facebook(
+				array(
+					'appId'  => Kohana::config('facebook')->app_id,
+					'secret' => Kohana::config('facebook')->secret,
+					//'cookie' => true, // enable optional cookie support
+				)
+			);
+		} else {
+			// Do class setup
+			$this->_facebook = new Facebook(
+				array(
+					'appId'  => $config['appId'],
+					'secret' => $config['secret'],
+					//'cookie' => true, // enable optional cookie support
+				)
+			);
+		}
 
 		$this->_user = $this->_facebook->getUser();
 	}
 
-	public static function instance()
+	public static function instance($config = null)
 	{
-		if ( ! isset(self::$_instance))
-			Kohana_Facebook::$_instance = new Kohana_Facebook;
+		if ( ! isset(self::$_instance)) {
+			Kohana_Facebook::$_instance = new Kohana_Facebook($config);
+		}
 
 		return Kohana_Facebook::$_instance;
 	}
